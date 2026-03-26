e5939dc69fa3706c91669c7e7f01458c1374b15b
DavidACM28 <dacasmore6@gmail.com>
2026-03-26 09:40:40 -0500

    feat(headquarters): add enum to map update validation errors

---
+++ src/main/java/pe/incubadora/backend/utils/UpdateSedeResult.java
@@ -0,0 +1,12 @@
+package pe.incubadora.backend.utils;
+
+public enum UpdateSedeResult {
+    SEDE_NOT_FOUND,
+    CODIGO_EMPTY,
+    NOMBRE_NOT_VALID,
+    CIUDAD_EMPTY,
+    DIRECCION_NOT_VALID,
+    RESPONSABLE_EMPTY,
+    ESTADO_NOT_VALID,
+    UPDATED
+}
